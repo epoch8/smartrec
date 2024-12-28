fcb300a901ff610c414ed32703f717e808cc4032
Andrey Tatarinov <a@tatarinov.co>
2024-12-28 17:32:36 +0400

    compare sets instead of exact lists

--- smartrec-lib/tests/test_fit_model_als_hot.py
+++ smartrec-lib/tests/test_fit_model_als_hot.py
@@ -53,7 +53,7 @@ def test_fit_als():
     )
     print(predictions)
     assert df_interactions.shape[0] == 100
-    assert predictions.item_ids == ["1253", "589", "3578"]
+    assert set(predictions.item_ids) == set(["1253", "589", "3578"])
     np.testing.assert_allclose(
         predictions.scores, [0.001144, 0.001138, 0.001135], atol=1e-5
     )
