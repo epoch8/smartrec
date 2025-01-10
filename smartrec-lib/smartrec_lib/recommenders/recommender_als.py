57b7568308baa050d404cdeae3befa9daaff92ba
Shakirov Renat <renatshk@gmail.com>
2025-01-10 12:38:19 +0300

    add-ligthfm

--- smartrec-lib/smartrec_lib/recommenders/recommender_als.py
+++ smartrec-lib/smartrec_lib/recommenders/recommender_als.py
@@ -77,17 +77,9 @@ class RecommenderALS(RecommenderModel):
         )
         self.model_cold_users.fit(dataset)
 
-        self.user_ids_hot = set(
-            self.dataset.user_id_map.convert_to_external(
-                self.dataset.interactions.df.user_id
-            )
-        )
+        self.user_ids_hot = set(self.dataset.user_id_map.convert_to_external(self.dataset.interactions.df.user_id))
         # Some item_ids may not be in interactions
-        self.item_ids_hot = set(
-            self.dataset.item_id_map.convert_to_external(
-                self.dataset.interactions.df.item_id
-            )
-        )
+        self.item_ids_hot = set(self.dataset.item_id_map.convert_to_external(self.dataset.interactions.df.item_id))
 
         logger.info("Base models trained.")
 
@@ -98,7 +90,7 @@ class RecommenderALS(RecommenderModel):
         filter_viewed: bool = True,
         items_to_recommend: Optional[List[int]] = None,
     ) -> RecomItems:  # Return type is a RecomItems
-        if items_to_recommend is None:
+        if isinstance(items_to_recommend, list) and len(items_to_recommend) == 0:
             return RecomItems(
                 item_ids=[],
                 scores=[],
@@ -108,22 +100,14 @@ class RecommenderALS(RecommenderModel):
         logger.info(f"Predicting for user {user_ids}")
         # sorting items that we have in interactions
         items_to_recommend_filtered = [
-            item_to_recommend
-            for item_to_recommend in items_to_recommend
-            if item_to_recommend in self.item_ids_hot
+            item_to_recommend for item_to_recommend in items_to_recommend if item_to_recommend in self.item_ids_hot
         ]
-
-        if len(items_to_recommend_filtered) == 0:
-            return RecomItems(
-                item_ids=[],
-                scores=[],
-                strategy="no_strategy_items_to_recommend_filtered_is_empty",
-            )
 
         recos: pd.DataFrame
 
         # user can be in the short memory, long memory or nowhere
         if user_ids in self.user_id_map.external_ids and user_ids in self.user_ids_hot:
+            logger.info("Hot user")
             recos = self.model_hot_users.recommend(
                 users=[user_ids],
                 dataset=self.dataset,
@@ -137,7 +121,7 @@ class RecommenderALS(RecommenderModel):
             )
             strategy = "model_hot_users"
         else:
-            logger.info("New user")
+            logger.info("Cold user")
             recos = self.model_cold_users.recommend(
                 users=[user_ids],
                 dataset=self.dataset,
@@ -184,9 +168,7 @@ class RecommenderALS(RecommenderModel):
             model_data=self.__dict__,
         )
         logger.info("Model saved successfully!")
-        clean_old_model_versions(
-            base_s3_url=base_s3_url, model_name=self.model_name, num_to_keep=num_to_keep
-        )
+        clean_old_model_versions(base_s3_url=base_s3_url, model_name=self.model_name, num_to_keep=num_to_keep)
         logger.info("Old models deleted!")
 
         return None
---
+++ smartrec-lib/tests/test_fit_model_lightfm_hot.py
@@ -0,0 +1,54 @@
+from datetime import datetime, timedelta
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+from rectools import Columns
+from rectools.dataset import Dataset
+
+from smartrec_lib.model import LighFMSettings
+from smartrec_lib.recommenders import RecommenderLightFM
+
+recsys_config_als = LighFMSettings(
+    RECOMMENDER_RANDOM_STATE=42,
+    RECOMMENDER_DAYS_THRESHOLD=14,
+)
+
+test_data = Path(__file__).parent
+
+
+def test_fit_als():
+    df_interactions = pd.read_csv(
+        test_data / "interactions.csv",
+        header=0,
+        names=[Columns.User, Columns.Item, Columns.Weight, Columns.Datetime],
+    )
+
+    train_dataset = Dataset.construct(
+        interactions_df=df_interactions,
+    )
+
+    model_name = "ligthfm_test_model"
+    model_version = str(int(datetime.now().timestamp()))
+    model = RecommenderLightFM(
+        model_name=model_name,
+        model_version=model_version,
+        recsys_config=recsys_config_als,
+    )
+
+    # metrics = model.calc_metrics(k=10, dataset=train_dataset)
+    model.train(train_dataset)
+
+    predictions = model.recommend(
+        user_ids=1,
+        top_n=3,
+        filter_viewed=True,
+        items_to_recommend=[589, 1253, 3578],
+    )
+    print(f"{predictions=}")
+    assert df_interactions.shape[0] == 100
+    assert set(predictions.item_ids) == set(["3578", "589", "1253"])
+    np.testing.assert_allclose(
+        predictions.scores, [-0.6149794459342957, -0.6518818736076355, -0.7153101563453674], atol=1e-5
+    )
+    assert predictions.strategy == "model_hot_and_cold_users"
